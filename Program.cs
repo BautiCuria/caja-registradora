ef66e541a2833f297b1308032e032589667b584c
BautiCuria <bautistacuria10@gmail.com>
2026-08-10 10:25:13 -0300

    etapa 1: bienvenida y datos del cajero

--- Program.cs
+++ Program.cs
@@ -1,1 +1,9 @@
 ﻿Console.WriteLine("Hello, World!");
+const string nombreComercio = "MAXIKIOSCO PIOLON";
+
+Console.WriteLine($"=== {nombreComercio} ===");
+
+Console.Write("Nombre del cajero: Pichi ");
+string nombreCajero = Console.ReadLine();
+
+Console.WriteLine($"Bienvenida, {nombreCajero}. Caja abierta.");
