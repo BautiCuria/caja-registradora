27d23b10e905cf82e9549f7f0254a2514a6dd814
BautiCuria <bautistacuria10@gmail.com>
2026-08-20 17:48:01 -0300

    Etapa 3: Carga multiple con menu

--- Program.cs
+++ Program.cs
@@ -7,11 +7,40 @@ string nombreCajero = Console.ReadLine();
 
 Console.WriteLine($"Bienvenid@, {nombreCajero}. Caja abierta.");
 
-Console.Write("Nombre del Producto");
-string nombreProducto = Console.ReadLine();
 
-Console.Write("Precio del producto: ");
-decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
+int cantidadProductos = 0;
+decimal totalventa = 0;
+string opcion = "";
 
-Console.WriteLine($"Se cargó el producto {nombreProducto} con un precio de ${precioProducto}");
+do
+{
+    Console.WriteLine("¿Que queres hacer?");
+    Console.WriteLine("1 - Cargar Producto");
+    Console.WriteLine("2 - Cerrar Venta");
+    opcion = Console.ReadLine();
 
+    switch (opcion)
+    {
+        case "1":
+            Console.Write("Nombre del Producto");
+            string nombreProducto = Console.ReadLine();
+
+            Console.Write("Precio del producto: ");
+            decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine($"Se cargó el producto {nombreProducto} con un precio de ${precioProducto}");
+            cantidadProductos++;
+            totalventa = totalventa + precioProducto;
+
+            break;
+
+        case "2":
+            Console.WriteLine("Cerrando la Venta");
+
+            break;
+        default:
+            Console.WriteLine("Opcion Invalida, intente denuevo.");
+            break;
+    }
+} while (opcion != "2");
+Console.WriteLine($"Venta Cerrada. Se cargaron {cantidadProductos} productos. Total a cobrar: ${totalventa}");
