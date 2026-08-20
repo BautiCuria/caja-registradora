171d3ab8fd7e2084ea676f5743e8b9a6bb352aa5
BautiCuria <bautistacuria10@gmail.com>
2026-08-20 16:56:18 -0300

    Etapa 2: carga de un producto

--- Program.cs
+++ Program.cs
@@ -6,3 +6,11 @@ Console.Write("Nombre del cajero: ");
 string nombreCajero = Console.ReadLine();
 
 Console.WriteLine($"Bienvenid@, {nombreCajero}. Caja abierta.");
+
+Console.Write("Nombre del Producto");
+string nombreProducto = Console.ReadLine();
+
+Console.Write("Precio del producto: ");
+decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
+
+Console.WriteLine($"Se cargó el producto {nombreProducto} con un precio de ${precioProducto}");
